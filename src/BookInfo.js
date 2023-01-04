50e1bd07a686aa8f98c652d6c64726bfa331837d
sakthiofficial <sakthiroky123@gmail.com>
2023-01-04 20:36:23 +0530

    all done

--- src/BookInfo.js
+++ src/BookInfo.js
@@ -39,7 +39,6 @@ export function BookInfo() {
 
 
 function BookCont({ data, id }) {
-  console.log(data);
   let [memdata, setmemdata] = useState()
   useEffect(() => {
     fetch(`https://6397271477359127a02e817e.mockapi.io/memebers`).then((res) => res.json()).then((val) => setmemdata(val))
@@ -91,6 +90,7 @@ function BookCont({ data, id }) {
   );
 }
 function BarrowCont({ barrow, setbarrow, barrowInfo, memdata, navigate, id }) {
+  // console.log(memdata);
   let memValidate = yup.object({
     memId: yup.number().max(memdata.length, `Member Id Within ${memdata.length}`).required(`Enter Id Within ${memdata.length}`)
 
@@ -102,51 +102,77 @@ function BarrowCont({ barrow, setbarrow, barrowInfo, memdata, navigate, id }) {
     },
     validationSchema: memValidate,
     onSubmit: (values) => {
-      putData(barrowInfo, id, values.memId, bookApi)
+      putBookData(barrowInfo, id, values.memId)
     }
   })
   let [memId, setmemId] = useState();
   let [count, setcount] = useState(0);
   let [memInfo, setmemInfo] = useContext(sample)
-  useEffect(() => {
-    fetch(`https://6397271477359127a02e817e.mockapi.io/memebers/${memId}`).then((res) => res.json()).then((val) => setmemInfo(val))
-  }, [memId])
+  // useEffect(() => {
+  //   fetch(`https://6397271477359127a02e817e.mockapi.io/memebers/${memId}`).then((res) => res.json()).then((val) => setmemInfo(val))
+  // }, [memId])
   let bookApi = "https://6397271477359127a02e817e.mockapi.io/books/";
   let memApi = "https://6397271477359127a02e817e.mockapi.io/memebers/";
+  // console.log(barrowInfo, id);
+
   let test = () => {
     console.log("sucess");
   }
-  let putData = ((arr, add, ele, api) => {
-
+  let putBookData = ((arr, add, ele) => {
+    // console.log(arr, ele);
     arr.map((val) => {
-      if (val != ele) {
+      if (val == ele) {
         console.log("already barrowed")
         setbarrow(!barrow)
 
-        return
 
       }
     })
 
-    fetch(`${api}${add}`, {
-      method: "PUT",
-      body: JSON.stringify({ barrowed: [...arr, parseInt(ele)] }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    }).then(() => {
-      if (count == 0) {
-        putData(memInfo.barrowed, memId, id, memApi)
-        count++
-      } else {
-        navigate(-1)
-        return
-      }
 
-    })
+    {
+      setbarrow ? fetch(`https://6397271477359127a02e817e.mockapi.io/books/${add}`, {
+        method: "PUT",
+        body: JSON.stringify({ barrowed: [...arr, parseInt(ele)] }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      }).then(() => {
+        // alert("Done");
+        // console.log("Done");
+        putUserData(ele, memdata, add)
 
-
+      }) : alert("This id already barrowed")
+    }
   })
+  function putUserData(id, arr, ele) {
+    console.log("Running");
+    arr.map(val => {
+      if (val.id == id) {
+        fetch(`https://6397271477359127a02e817e.mockapi.io/memebers/${id}`, {
+          method: "PUT",
+          body: JSON.stringify({ barrowed: [...val.barrowed, parseInt(ele)] }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        }).then(() => {
+          navigate(-1)
+          console.log("clicked");
+
+          setbarrow(!barrow)
+          return
+
+        })
+      }
+    })
+    console.log("hello");
+
+
+  }
+
+
+
+
 
   return (
     <div className="barrow-cont" onDoubleClick={() => setbarrow(false)}>
@@ -210,49 +236,49 @@ function MemInfoTable({ setshowmem, memData, id, barrowInfo }) {
 
                   <TableRow hover role="checkbox">
                     <div className="barrow-msg">
-                    <h1 >No One  Barrowed This Book</h1>
+                      <h1 >No One  Barrowed This Book</h1>
 
-                  </div>
-                </TableRow >
-              
-            :null}
-              {memData.map((mem) => barrowInfo.map((val, index) => {
-                {
+                    </div>
+                  </TableRow >
 
-                  if (val == mem.id) {
-                    return (
-                      <TableRow hover role="checkbox">
+                  : null}
+                {memData.map((mem) => barrowInfo.map((val, index) => {
+                  {
 
-                        <TableCell>
-                          {mem.memname}
-                        </TableCell>
-                        <TableCell>
-                          {mem.email}
-                        </TableCell> <TableCell>
-                          {mem.phone}
-                        </TableCell>
-                        <TableCell>
-                          <Button color="error" onClick={() => postData(index)}>return</Button>
-                        </TableCell>
-                      </TableRow>
-                    )
+                    if (val == mem.id) {
+                      return (
+                        <TableRow hover role="checkbox">
+
+                          <TableCell>
+                            {mem.memname}
+                          </TableCell>
+                          <TableCell>
+                            {mem.email}
+                          </TableCell> <TableCell>
+                            {mem.phone}
+                          </TableCell>
+                          <TableCell>
+                            <Button color="error" onClick={() => postData(index)}>return</Button>
+                          </TableCell>
+                        </TableRow>
+                      )
+                    }
+
                   }
-
-                }
-              }))}
+                }))}
 
 
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Button onClick={() => setshowmem(false)}>back</Button>
-        <TablePagination
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Button onClick={() => setshowmem(false)}>back</Button>
+          <TablePagination
 
-        />
+          />
 
-      </Paper>
+        </Paper>
 
-    </div>
+      </div>
     </div >
   )
 }
